51bea6281e81b7de422b825df8ffd1f658cd179e
DanielCoelhoOliveira97 <sco@siteimprove.com>
2025-02-21 12:41:04 +0000

    INS-3273 Remove sections and dashboards folder

--- PluginComposer.cs
+++ PluginComposer.cs
@@ -1,14 +1,12 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Siteimprove.Umbraco13.Plugin.Dashboards;
 using Siteimprove.Umbraco13.Plugin.Middlewares;
-using Siteimprove.Umbraco13.Plugin.Sections;
 using Siteimprove.Umbraco13.Plugin.Services;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Core.Sections;
 using Umbraco.Cms.Web.Common.ApplicationBuilder;
+using Umbraco.Cms.Core.Dashboards;
 
 namespace Siteimprove.Umbraco13.Plugin
 {
@@ -29,5 +27,23 @@ namespace Siteimprove.Umbraco13.Plugin
 						postPipeline: app => app.UseMiddleware<PreviewScriptInjectionMiddleware>()
 			)));
 		}
+	}
+
+	public class SiteimprovePublicUrlDashboard : IDashboard
+	{
+		public string[] Sections => new[] { "siteimprovePublicUrlSection" };
+
+		public IAccessRule[] AccessRules => new[] { new AccessRule { Type = AccessRuleType.Grant, Value = "10" } };
+
+		public string? Alias => "siteimprovePublicUrlDashboard";
+
+		public string? View => "/App_Plugins/Siteimprove/views/publicUrlSection.html";
+	}
+
+	public class SiteimprovePublicUrlSection : ISection
+	{
+		public string Alias => "siteimprovePublicUrlSection";
+		public string Name => "Public URL";
+		public string Icon => "icon-calculator";
 	}
 }
